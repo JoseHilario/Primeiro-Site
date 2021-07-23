00d54f84d99f05d6bdd3d77d8dc6c90ab0610f7e
José Hilário <hilariofrotafilho@gmail.com>
2021-07-22 21:42:48 -0400

    js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const nav = document.querySelector('header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
